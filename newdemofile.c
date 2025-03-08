f2e547ed17fcd4607ae5f87dc0dfb8646e86bf31
Bhuvana <bhuvana.pbk@gmail.com>
2025-03-08 15:19:30 +0530

    changes in branch

--- newdemofile.c
+++ newdemofile.c
@@ -2,5 +2,7 @@
 int main(){
     printf("hello world");
     printf("this is my practice work that im preforming");
+    //adding more data from practice branch
+    printf("to practice the merge branch");
 return 0;
 }
